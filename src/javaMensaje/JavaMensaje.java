95f4b2c4e6eda61f659a6ae94979d32496cbf0b7
Administrador <Administrador@2DAM01>
2023-11-10 16:59:49 +0100

    Se ha puesto nombre a la ventana

--- src/javaMensaje/JavaMensaje.java
+++ src/javaMensaje/JavaMensaje.java
@@ -39,6 +39,7 @@ public class JavaMensaje {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
+		frame.setTitle("Ejemplo Remoto");
 	}
 
 }
